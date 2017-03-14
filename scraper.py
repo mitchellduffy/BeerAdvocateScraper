468d7d953a2446ed12b709438a10649cd511eae3
mihess <mirco.hess@googlemail.com>
2017-03-14 23:53:44 +0100

    multiple search pages; class interface

--- scraper.py
+++ scraper.py
@@ -1,27 +1,45 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-soup = BeautifulSoup(requests.get("http://beeradvocate.com/search?q=ipa&qt=beer").content)
+class Scraper:
+    def __init__(self):
+        self.url_beer_temp = "https://www.beeradvocate.com/search/?start=%d&q=ipa&qt=beer&retired=N"
 
-results = soup.findAll('ul')
+    def get_beer_soup(self, start_number=0):
+        self.url_beer_ = self.url_beer_temp % start_number
+        return BeautifulSoup(requests.get(self.url_beer_).content) 
 
-# manually identified item 20 
-beer_links = results[20].findAll('li')
-links = []
-for link in beer_links:
-    links.append("http://beeradvocate.com%s" % (link.find('a')['href']))
-    
+    def get_beer_dict(self, beer_soup, min_total_ratings=100):
+        results = beer_soup.findAll('ul')
+        beer_links = results[20].findAll('li')
 
-beer_list = {}
-for beer in links:
-    soup = BeautifulSoup(requests.get(beer).content)
-    heading = soup.find('h1').getText()
-    heading = heading.split('|')
-    name = heading[0]
-    brewery = heading[1]
-    beer_list[name] = {}
-    beer_list[name]['brewery'] = brewery
-    beer_list[name]['link'] = beer
+        links = []
+        for link in beer_links:
+            links.append("http://beeradvocate.com%s" % (link.find('a')['href']))
+            
+        beer_dict = {}
+        for beer in links:
+            soup = BeautifulSoup(requests.get(beer).content)
+
+            ratings = soup.find('span', attrs={'class':'ba-ratings'}).getText()
+            if ratings >= min_total_ratings:
+                avg = soup.find('span', attrs={'class':'ba-ravg'}).getText()
+                heading = soup.find('h1').getText()
+                heading = heading.split('|')
+                name = heading[0]
+                brewery = heading[1]
+                beer_dict[name] = {}
+                beer_dict[name]['brewery'] = brewery
+                beer_dict[name]['link'] = beer
+                beer_dict[name]['ratings'] = ratings
+                beer_dict[name]['average'] = avg
 
 
-print beer_list
+
+scrap = Scraper()
+
+soup = scrap.get_beer_soup(0)
+beer_dict = scrap.get_beer_dict(soup)
+
+print beer_dict
+           
